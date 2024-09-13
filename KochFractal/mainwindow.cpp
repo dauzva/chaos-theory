12f478d8780694f607c723ddc4018afeed3fbbc6
Darijus Daužvardis <dauzva@gmail.com>
2024-09-13 17:30:58 +0300

    QPainter init and base form UI

--- KochFractal/mainwindow.cpp
+++ KochFractal/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPainter>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +12,16 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QRect rect = ui->canvas->geometry();
+    int ax, ay, aw, ah;
+    rect.getRect(&ax, &ay, &aw, &ah);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.fillRect(rect, Qt::white);
+    painter.setPen(QPen(Qt::black, 1));
+    painter.drawLine(ax, ay+ah/2, ax+aw, ay+ah/2);
 }
--- KochFractal/mainwindow.h
+++ KochFractal/mainwindow.h
@@ -16,6 +16,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void paintEvent(QPaintEvent *event) override;
 
 private:
     Ui::MainWindow *ui;
