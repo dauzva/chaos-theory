aa07f5d4c8d7d747b2aec16098d34c4b3d6df11f
Darijus Daužvardis <dauzva@gmail.com>
2024-09-19 20:04:58 +0300

    fully working koch fractal generation

--- KochFractal/mainwindow.cpp
+++ KochFractal/mainwindow.cpp
@@ -34,7 +34,8 @@ void MainWindow::paintEvent(QPaintEvent *event)
 
 void MainWindow::on_iterateBtn_clicked()
 {
-    ui->iterationLabel->setText(QString::number(++iterationLevel));
+    if(iterationLevel < 5)
+        ui->iterationLabel->setText(QString::number(++iterationLevel));
 }
 
 void MainWindow::updateTimer()
@@ -61,9 +62,26 @@ void MainWindow::updateKochPoints()
         for(int j = 0; j < iterationLevel; ++j)
         {
             kochPoints.clear();
-            for(int i = 0; i < savedKochPoints.size(); ++i)
+            for(int i = 0; i < savedKochPoints.size()-1; ++i)
             {
-                //
+                QPointF p = savedKochPoints.at(i);
+                kochPoints.append(p);
+                QPointF p1 = savedKochPoints.at(i+1);
+                QLineF l = QLineF(p,p1);
+                QLineF l1 = QLineF(p, l.pointAt((qreal)1/3));
+                QLineF l21 = QLineF(l.pointAt((qreal)1/3), l.pointAt((qreal)1/2));
+                QLineF l22 = QLineF(l.pointAt((qreal)1/3), l.pointAt((qreal)2/3)).normalVector();
+                QLineF l2 = QLineF(l1.p2(), QPointF(l21.p2().x()+l22.p2().x()-l1.p2().x(), l21.p2().y()+l22.p2().y()-l1.p2().y()));
+                QLineF l3 = QLineF(l2.p2(), l.pointAt((qreal)2/3));
+                QLineF l4 = QLineF(l.pointAt((qreal)2/3), p1);
+
+                kochPoints.append(l1.p2());
+                kochPoints.append(l1.p2());
+                kochPoints.append(l2.p2());
+                kochPoints.append(l2.p2());
+                kochPoints.append(l3.p2());
+                kochPoints.append(l3.p2());
+                kochPoints.append(l4.p2());
             }
             savedKochPoints.resize(kochPoints.size());
             savedKochPoints = kochPoints;
