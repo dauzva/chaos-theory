5f4d94ec355ed15db1299e7d616f4a0f1caf4442
Darijus Daužvardis <dauzva@gmail.com>
2024-11-14 23:57:47 +0200

    reused Koch Curve code

--- KochCurveAnimation/mainwindow.cpp
+++ KochCurveAnimation/mainwindow.cpp
@@ -1,14 +1,136 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPainter>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->canvas->resize(this->width()-400, this->height());
+
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(updateTimer()));
+    timer->start(10); // Timer fires every 1 milliseconds
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    QRect rect = ui->canvas->geometry();
+    int ax, ay, aw, ah;
+    rect.getRect(&ax, &ay, &aw, &ah);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.fillRect(rect, Qt::white);
+    painter.setPen(QPen(Qt::black, 1));
+    painter.drawLines(kochPoints);
+    // drawing a line of a shortest distance
+
+    if(isMouse)
+    {
+        if(!rect.contains(mapFromGlobal(QCursor::pos())))
+            isMouse = false;
+        painter.setPen(QPen(Qt::red, 1));
+        painter.drawLine(bestPoint, mapFromGlobal(QCursor::pos()));
+        ui->distanceLabel->setText(QString::number((QLineF(bestPoint, mapFromGlobal(QCursor::pos())).length()), 'f', 2)+" px");
+    }
+    else
+        ui->distanceLabel->setText(QString::number(0)+" px");
+}
+
+void MainWindow::on_iterateBtn_clicked()
+{
+    if(iterationLevel < 5)
+        ui->iterationLabel->setText(QString::number(++iterationLevel));
+}
+
+void MainWindow::on_resetBtn_clicked()
+{
+    iterationLevel = 0;
+    ui->iterationLabel->setText(QString::number(0));
+}
+
+void MainWindow::updateTimer()
+{
+    this->update();
+    this->updateKochPoints();
+    if(isMouse)
+        this->findShortest();
+}
+
+void MainWindow::updateKochPoints()
+{
+    kochPoints.clear();
+    QRect rect = ui->canvas->geometry();
+    int ax, ay, aw, ah;
+    rect.getRect(&ax, &ay, &aw, &ah);
+
+    kochPoints.append(QPointF(ax, 2*(ay+ah)/3));
+    kochPoints.append(QPointF(ax+aw, 2*(ay+ah)/3));
+    savedKochPoints = kochPoints;
+    if(iterationLevel != 0)
+    {
+        for(int j = 0; j < iterationLevel; ++j)
+        {
+            kochPoints.clear();
+            for(int i = 0; i < savedKochPoints.size()-1; ++i)
+            {
+                QPointF p = savedKochPoints.at(i);
+                kochPoints.append(p);
+                QPointF p1 = savedKochPoints.at(i+1);
+                QLineF l = QLineF(p,p1);
+                QLineF l1 = QLineF(p, l.pointAt((qreal)1/3));
+                QLineF l21 = QLineF(l.pointAt((qreal)1/3), l.pointAt((qreal)1/2));
+                QLineF l22 = QLineF(l.pointAt((qreal)1/3), l.pointAt((qreal)2/3)).normalVector();
+                QLineF l2 = QLineF(l1.p2(), QPointF(l21.p2().x()+l22.p2().x()-l1.p2().x(), l21.p2().y()+l22.p2().y()-l1.p2().y()));
+                QLineF l3 = QLineF(l2.p2(), l.pointAt((qreal)2/3));
+                QLineF l4 = QLineF(l.pointAt((qreal)2/3), p1);
+
+                kochPoints.append(l1.p2());
+                kochPoints.append(l1.p2());
+                kochPoints.append(l2.p2());
+                kochPoints.append(l2.p2());
+                kochPoints.append(l3.p2());
+                kochPoints.append(l3.p2());
+                kochPoints.append(l4.p2());
+            }
+            savedKochPoints.resize(kochPoints.size());
+            savedKochPoints = kochPoints;
+        }
+    }
+}
+
+void MainWindow::findShortest()
+{
+    bestPoint = kochPoints[0];
+    QLineF l = QLineF(bestPoint, mapFromGlobal(QCursor::pos()));
+    for(int i = 0; i < kochPoints.size(); ++i)
+    {
+        QLineF temp = QLineF(kochPoints[i], mapFromGlobal(QCursor::pos()));
+        if(temp.length() < l.length())
+        {
+            l = temp;
+            bestPoint = kochPoints[i];
+        }
+    }
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+    QRect rect = ui->canvas->geometry();
+    if(rect.contains(mapFromGlobal(QCursor::pos())))
+        isMouse=true;
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *e)
+{
+    isMouse=false;
+}
+
+
--- KochCurveAnimation/mainwindow.h
+++ KochCurveAnimation/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,8 +17,28 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void paintEvent(QPaintEvent *event) override;
+
+public slots:
+    void updateTimer();
+    void updateKochPoints();
+    void findShortest();
+
+private slots:
+    void on_iterateBtn_clicked();
+    void on_resetBtn_clicked();
+    void mousePressEvent(QMouseEvent *e) override;
+    void mouseReleaseEvent(QMouseEvent *e) override;
 
 private:
     Ui::MainWindow *ui;
+    QTimer* timer;
+    int iterationLevel = 0;
+    int kochSize = 2;
+    QList<QPointF> kochPoints;
+    QList<QPointF> savedKochPoints;
+    QPointF bestPoint;
+    bool isMouse = false;
+
 };
 #endif // MAINWINDOW_H
